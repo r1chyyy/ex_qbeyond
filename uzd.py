7b9ac1c8409219fe868ec46680eaae1853b4cc97
r1chyyy <rihardsge@gmail.com>
2022-08-29 01:40:56 +0300

    double check

--- uzd.py
+++ uzd.py
@@ -1,3 +1,23 @@
 import json
 
+with open("calc.json", "r") as read_file:
+    data = json.load(read_file)
 
+uzd = {
+    
+    "input1": 2,
+    "commment":"accept any number",
+    "input2": 3,
+    "commment":"accept any number",
+    "operator":"+",
+    "comment":"options here: '+','-','*','/' "
+
+}
+
+data = json.loads(uzd)
+
+unformatted_json = json.dumps(uzd)
+print(unformatted_json)  
+
+
+#    file.write(jsonResponse)
