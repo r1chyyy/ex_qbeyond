a0b261bfe638dfe0e17d9aafcfb588db249addbc
r1chyyy <rihardsge@gmail.com>
2022-09-03 02:18:09 +0300

    Created api routes and requirements.txt

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Qbeyond Uzdevums
+
+### Getting started
+    1.Create a virtual Environment
+    2.Start the virtual env. `source env/bin/activate`
+    3.Install packages by running `pip install -r requirements.txt`
+    4.Run the app with `python3 ./app.py`
+
+### Saving libraries
+write `pip freeze > requirements.txt` to save libraries for package manager use.
+
+
+
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask,request,g,Response, jsonify
+from flask_expects_json import expects_json
+
+# Creates api app
+app =   Flask(__name__)
+
+# Validation schema for response
+schema = {
+  "type": "object",
+  "properties": {
+    "input1": { "type": "number" },
+    "input2": { "type": "number" },
+    "operator": { "type": "string" },
+  },
+  'required': ['input1', 'input2', 'operator']
+}
+
+# Calculation function
+def calculate(input1, input2, operator):
+
+    if operator == '+':
+        return formatRes(input1 + input2)
+
+    elif operator == '-':
+        return formatRes(input1 + input2)
+
+    elif operator == '*':
+        return formatRes(input1 * input2)
+
+    elif operator == '/':
+        return formatRes(input1 / input2)
+    else: 
+        # On wrong operator type return bad response
+        return Response("Wrong operator type",status=400)
+
+# Formatting response to json
+def formatRes(res):
+    return jsonify({'result': res})
+
+# Defining api route
+@app.route('/calculate', methods = ['GET'])
+@expects_json(schema)
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = request.json
+        res = calculate(data['input1'], data['input2'], data['operator'])
+        return res
+
+  
+if __name__=='__main__':
+    app.run(debug=True)
